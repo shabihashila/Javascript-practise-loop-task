4f75ab06347a93f5fc587cbfa7853cc464a95260
HP <HP@DESKTOP-8E0FCDH>
2024-09-15 21:08:27 +0600

    sum-of-odd-number.js

--- sum-of-odd-number.js
+++ sum-of-odd-number.js
@@ -5,13 +5,31 @@ Subtask-1:
 Display sum of all the odd numbers from 81 to 131.
 
  */
-let num = 81;
+// let num = 81;
+// let sum = 0;
+// while (num >= 81 && num <= 131) {
+//   if (num % 2 !== 0) {
+//     console.log("odd number:", num);
+//     sum = sum + num;
+//     console.log("sum of odd :", sum);
+//   }
+//   num++;
+// }
+/***
+
+Subtask-2:
+
+Display sum of all the even numbers from 206 to 311.
+
+ */
+
+let number = 207;
 let sum = 0;
-while (num >= 81 && num <= 131) {
-  if (num % 2 !== 0) {
-    console.log("odd number:", num);
-    sum = sum + num;
-    console.log("sum of odd :", sum);
+while (number >= 207 && number <= 311) {
+  if (number % 2 === 0) {
+    console.log("even number:", number);
+    sum = sum + number;
+    console.log("sum of even:", sum);
   }
-  num++;
+  number++;
 }
