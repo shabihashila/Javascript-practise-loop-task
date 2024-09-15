60c7528804846e0f9de66bb261af075676e8fe2a
HP <HP@DESKTOP-8E0FCDH>
2024-09-16 01:17:04 +0600

    odd-even-for.js

---
+++ for.js
@@ -0,0 +1,15 @@
+// for loop
+
+// for(  ;  ;   ){
+//     loop statement
+// }
+
+// let num = 1;
+// while (num <= 10) {
+//   console.log(num);
+//   num++;
+// }
+
+for (let num = 1; num <= 5; num++) {
+  console.log(num);
+}
---
+++ odd-even-for.js
@@ -0,0 +1,11 @@
+for (let num = 1; num <= 6; num++) {
+  if (num % 2 === 0) {
+    console.log("even number:", num);
+  }
+}
+
+for (let num = 1; num <= 6; num++) {
+  if (num % 2 !== 0) {
+    console.log("odd number:", num);
+  }
+}
