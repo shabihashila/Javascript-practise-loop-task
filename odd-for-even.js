33e28497c81ae6842da89043911746a182ed3d11
HP <HP@DESKTOP-8E0FCDH>
2024-09-16 03:49:37 +0600

    odd-for-even.js

---
+++ odd-for-even.js
@@ -0,0 +1,26 @@
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+// use for loop
+for (let i = 61; i >= 61 && i <= 100; i++) {
+  if (i % 2 !== 0) {
+    console.log("odd num:", i);
+  }
+}
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+for (num = 78; num >= 78 && num <= 98; num++) {
+  if (num % 2 === 0) {
+    console.log("even num: ", num);
+  }
+}
