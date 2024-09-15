b9710f0c799486b518df71c4688e556639557a84
HP <HP@DESKTOP-8E0FCDH>
2024-09-16 03:31:16 +0600

    simple-for.js

---
+++ simple-for.js
@@ -0,0 +1,11 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+for (let message = 1; message <= 60; message++) {
+  console.log(
+    "I will invest at least 6 hrs every single day for next 60 days!"
+  );
+}
