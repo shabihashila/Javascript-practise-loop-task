4d36cf442f5deabba81a651b90399223c49d4a2f
HP <HP@DESKTOP-8E0FCDH>
2024-09-15 18:49:36 +0600

    commitment.js

---
+++ commitment.js
@@ -0,0 +1,15 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+/*programming hero*/
+
+let message = 1;
+while (message <= 60) {
+  console.log(
+    "I will invest at least 6 hrs every single day for next 60 days!"
+  );
+  message++;
+}
