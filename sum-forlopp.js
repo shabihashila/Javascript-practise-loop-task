c70e85353c6abdd051ab304af79d1fcfd2aa2312
HP <HP@DESKTOP-8E0FCDH>
2024-09-16 02:47:22 +0600

    decreamental-while.js

---
+++ sum-forlopp.js
@@ -0,0 +1,13 @@
+// let i = 1;
+// let sum = 0;
+// while (i <= 10) {
+//   console.log(i);
+//   sum = sum + i;
+//   i++;
+// }
+// console.log("sum of the number:", sum);
+let sum = 0;
+for (let i = 1; i <= 20; i = i + 1) {
+  sum = sum + i;
+}
+console.log("sum of the number:", sum);
