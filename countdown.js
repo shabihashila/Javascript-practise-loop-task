bdfc39120b72351d1f468db1f746c6baa581d0f3
HP <HP@DESKTOP-8E0FCDH>
2024-09-16 19:18:10 +0600

    countdown.js

--- countdown.js
+++ countdown.js
@@ -6,3 +6,16 @@ Implement a countdown timer that counts down from 81 to 65.
 for (let i = 81; i >= 65; i--) {
   console.log(i);
 }
+console.log("using for loop");
+
+/***
+
+Implement a countdown timer that counts down from 21 to 15.
+
+ */
+let n = 21;
+while (n >= 15) {
+  console.log(n);
+  n--;
+}
+console.log("using while loop");
