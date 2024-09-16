2c443b81f240ff62f4cc1775adbb8e5390fa0cdc
HP <HP@DESKTOP-8E0FCDH>
2024-09-16 19:05:11 +0600

    namota.js

---
+++ gun.js
@@ -0,0 +1,11 @@
+/***
+
+Generate a multiplication table for number 9
+
+ */
+
+let number = 5;
+
+for (let i = 1; i <= 10; i++) {
+  console.log(number + "x" + i + "=" + number * i);
+}
---
+++ namota.js
@@ -0,0 +1,9 @@
+// As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+let num = 5;
+let i = 1;
+let result = num * i;
+while (i <= 10) {
+  console.log(num + "x" + i + "=" + result);
+  i++;
+}
