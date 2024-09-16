450f25feb0a5e8ee00b159209ef3bf17442b0a16
HP <HP@DESKTOP-8E0FCDH>
2024-09-16 20:07:47 +0600

    hard1.js

---
+++ hard.js
@@ -0,0 +1,13 @@
+/* 
+Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
+*/
+
+let sum = 0; 
+let i = 1;    
+
+while (sum < 100) {
+  sum += i;  
+  i++;        
+}
+
+console.log(`Final sum is ${sum}`);
